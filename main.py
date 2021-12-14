3f93fea2eef5676154c12a72a46edf30262a7787
Ben Nguyen <benedicn@gmail.com>
2021-12-13 21:53:14 -0600

    more update

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import time
 
 
 class Snake:
-    def __init__(self, parent_screen):
-        self.parent_screen = parent_screen
+    def __init__(self, surface):
+        self.parent_screen = surface
         self.block = pygame.image.load(
             "C:\\Users\\bened\\Desktop\\Projects\\Snake_Game\\resources\\block.jpg"
         ).convert()
 
-        self.x = 200
-        self.y = 200
+        self.x = 300
+        self.y = 300
         self.direction = "down"
 
     def move_left(self):
@@ -27,14 +27,14 @@ class Snake:
         self.direction = "down"
 
     def walk(self):
-        if self.direction == "up":
-            self.y -= 10
-        if self.direction == "down":
-            self.y += 10
         if self.direction == "left":
             self.x -= 10
         if self.direction == "right":
             self.x += 10
+        if self.direction == "up":
+            self.y -= 10
+        if self.direction == "down":
+            self.y += 10
 
         self.draw()
 
@@ -48,7 +48,6 @@ class Game:
     def __init__(self):
         pygame.init()
         self.surface = pygame.display.set_mode((800, 800))
-        self.surface.fill((128, 128, 128))
         self.snake = Snake(self.surface)
         self.snake.draw()
 
@@ -58,21 +57,27 @@ class Game:
         while running:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
-                    if event.type == K_ESCAPE:
+                    if event.key == K_ESCAPE:
                         running = False
-                    if event.key == K_UP:
-                        self.snake.move_up()
-                    if event.key == K_DOWN:
-                        self.snake.move_down()
+
                     if event.key == K_LEFT:
                         self.snake.move_left()
+
                     if event.key == K_RIGHT:
                         self.snake.move_right()
+
+                    if event.key == K_UP:
+                        self.snake.move_up()
+
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+
                 elif event.type == QUIT:
                     running = False
 
-        self.snake.walk()
-        time.sleep(0.2)
+            self.snake.walk()
+
+            time.sleep(0.1)
 
 
 if __name__ == "__main__":
